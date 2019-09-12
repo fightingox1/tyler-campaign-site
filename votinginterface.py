f948709410c27639b0f44807fe93659c60303f7c
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-09-12 10:05:47 -0400

    Update votinginterface.py

--- votinginterface.py
+++ votinginterface.py
@@ -1,7 +1,7 @@
 f = "1"
 while f == "1":
   a = input("click y for voting for me type n if your not")
-  if a === "y":
+  if a == "y":
     print("thankyou for voting for tyler")
   if a == "y":
     print("thank you for taing the poll we hope we can change your mind on the decision")
